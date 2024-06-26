8635ce64ce00c3f6f7eda0e54629b8b773400494
Ssgithub159 <sarushekh159@gmail.com>
2024-06-26 11:17:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("6063555621", "6063555621").split()))
+OWNER_ID = int(getenv("1331161476"))
+MONGO_URL = getenv("mongodb+srv://Danger:dangerbadmosh@danger2.kxpnfoe.mongodb.net/?retryWrites=true&w=majority&appName=DANGER2")
+BOT_TOKEN = getenv("7051832270:AAFWNbcCg0EZirJnzsztIwXX7sqrit25Sp0", "7051832270:AAFWNbcCg0EZirJnzsztIwXX7sqrit25Sp0")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/Ssgithub159/D-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
